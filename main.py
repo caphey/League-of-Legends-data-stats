295c46749f778a6af04c947078a4914e6fd7f0bd
caphey <99432180+caphey@users.noreply.github.com>
2024-09-30 14:32:43 +0200

    Update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from config import Config
 from services import riot
-from database import save_user_data, get_user_data
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,20 +17,13 @@ def submit():
     game_name = request.form.get('game_name')
     tag_line = request.form.get('tag_line')
 
-    # try:
-    existing_user = get_user_data(game_name, tag_line)
-    new_user_data = riot.get_user_data(game_name, tag_line)
-
-    # if existing_user != new_user_data:
-    #     user_data = save_user_data(new_user_data)
-    # else:
-    #     user_data = existing_user
-
-    session['user_data'] = new_user_data
-    return redirect(url_for('result'))
-    # except Exception as e:
-    #     # Log l'erreur
-    #     return render_template('error.html', message="Une erreur s'est produite. Veuillez réessayer plus tard.")
+    try:
+        new_user_data = riot.get_user_data(game_name, tag_line)
+        session['user_data'] = new_user_data
+        return redirect(url_for('result'))
+    except Exception as e:
+        # Log l'erreur
+        return render_template('error.html', message="Une erreur s'est produite. Veuillez réessayer plus tard.")
 
 
 @app.route('/result')
